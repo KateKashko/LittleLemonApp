c77f71123ea020b03ec83823144634047124914f
Katerina <katekashkobiz@gmail.com>
2024-07-20 21:39:26 +0300

    Initial Commit

---
+++ LittleLemonApp/LittleLemonAppApp.swift
@@ -0,0 +1,20 @@
+//
+//  LittleLemonAppApp.swift
+//  LittleLemonApp
+//
+//  Created by Kate Kashko on 20.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonAppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
